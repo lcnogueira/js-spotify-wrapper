4a735e4adc9804c947ee3b61707f2c796b9612b2
Luiz Claudio <luizcns18@gmail.com>
2020-01-27 11:14:54 -0300

    Bump version 1.0.0

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const TOKEN_API = 'YOUR_TOKEN_HERE';
+const TOKEN_API = 'BQBfg_9XcUr3DRGLVyOFSEfxcZ9ZuSZXeVvLkl45Z_vMlINklpnLDYuUTwCJXFDoQNodA0fafbM6eiRdmdN9oem6TRpJrLOnyqIkrIVSk-a6BVCHSvLf8FG-o5Czb5aOP9pWP3A';
 
 export const API_URL = 'https://api.spotify.com/v1';
 
